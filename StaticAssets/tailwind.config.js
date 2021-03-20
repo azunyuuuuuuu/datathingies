309ceaa96bbdc3ee1ec0602d135b389b10e4087b
azunyuuuuuuu <azunyuuuuuuu@users.noreply.github.com>
2021-03-20 11:36:10 +0100

    restored `tailwind.config.js` file

--- StaticAssets/tailwind.config.js
+++ StaticAssets/tailwind.config.js
@@ -1,8 +1,36 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray': colors.trueGray,
+        'blue-gray': colors.blueGray,
+        'cool-gray': colors.coolGray,
+        'default-gray': colors.gray,
+        'true-gray': colors.trueGray,
+        'warm-gray': colors.warmGray,
+        'red': colors.red,
+        'orange': colors.orange,
+        'amber': colors.amber,
+        'yellow': colors.yellow,
+        'lime': colors.lime,
+        'green': colors.green,
+        'emerald': colors.emerald,
+        'teal': colors.teal,
+        'cyan': colors.cyan,
+        'light-blue': colors.lightBlue,
+        'blue': colors.blue,
+        'indigo': colors.indigo,
+        'violet': colors.violet,
+        'purple': colors.purple,
+        'fuchsia': colors.fuchsia,
+        'pink': colors.pink,
+        'rose': colors.rose,
+      },
+    },
   },
   variants: {
     extend: {},
